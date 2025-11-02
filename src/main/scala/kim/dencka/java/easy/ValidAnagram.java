6e6255f5e06cd899c3f47968cd2930385b0172d9
Denis Kim <densungkim@gmail.com>
2025-11-02 21:55:06 +0500

    [new] Valid Anagram (java)

---
+++ src/main/scala/kim/dencka/java/easy/ValidAnagram.java
@@ -0,0 +1,24 @@
+package kim.dencka.java.easy;
+
+public class ValidAnagram {
+    public static void main(String[] args) {
+        System.out.println(isAnagram("anagram", "nagaram")); // true
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for (int c : count) {
+            if (c != 0) return false;
+        }
+
+        return true;
+    }
+}
