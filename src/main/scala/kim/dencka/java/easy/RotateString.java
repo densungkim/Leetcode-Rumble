cf155daf47b7cd71cfc91759323a5b71463cfc57
Denis Kim <densungkim@gmail.com>
2025-11-12 20:16:21 +0500

    [new] Rotate String (java)

---
+++ src/main/scala/kim/dencka/java/easy/RotateString.java
@@ -0,0 +1,12 @@
+package kim.dencka.java.easy;
+
+public class RotateString {
+    public static void main(String[] args) {
+        System.out.println(rotateString("abcde", "cdeab")); // true
+    }
+
+    public static boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) return false;
+        return (s + s).contains(goal);
+    }
+}
